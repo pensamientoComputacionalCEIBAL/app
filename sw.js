e808c2099447af62ecd1e75236222f4e9478a406
pensamientoComputacionalCEIBAL <71598555+pensamientoComputacionalCEIBAL@users.noreply.github.com>
2026-07-13 15:38:27 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'genially-pwa-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  // Solo gestionamos nuestro propio dominio; el contenido de Genially se pide siempre a la red.
+  if (url.origin !== self.location.origin) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  );
+});
